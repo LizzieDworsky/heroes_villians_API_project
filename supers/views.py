003ae0e52bbb3313d4285b356b20045a9453fc3d
LizzieDworsky <amydworsky15@gmail.com>
2022-05-26 12:34:40 -0600

    began building out the supers views file, imports and functions, both incomplete

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Super
 
 # Create your views here.
+
+@api_view(['GET'])
+def supers_list(request):
+    pass
+
+
+@api_view(['GET'])
+def super_details(request):
+    pass

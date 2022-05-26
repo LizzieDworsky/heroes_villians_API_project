188ed197fb7804ad7df2083a97abfa7bd6c80792
LizzieDworsky <amydworsky15@gmail.com>
2022-05-26 14:10:22 -0600

    added the POST method to views supers_list function

--- supers/views.py
+++ supers/views.py
@@ -7,12 +7,17 @@ from .serializers import SuperSerializer
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
         supers = Super.objects.all()
         serializer = SuperSerializer(supers, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 
@@ -21,4 +26,4 @@ def super_details(request, pk):
     if request.method == 'GET':
         single_super = get_object_or_404(Super, pk=pk)
         serializer = SuperSerializer(single_super)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)

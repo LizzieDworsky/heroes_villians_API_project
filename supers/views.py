4da0dc1c7277848c187f3ff8057b5974dceb43bc
LizzieDworsky <amydworsky15@gmail.com>
2022-05-27 12:30:40 -0600

    added a custom dictionary response to the views supers_list function

--- supers/views.py
+++ supers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from super_types.models import SuperType
 from .models import Super
 from .serializers import SuperSerializer
 
@@ -11,11 +12,23 @@ from .serializers import SuperSerializer
 def supers_list(request):
     if request.method == 'GET':
         type_param = request.query_params.get('type')
-        supers = Super.objects.all()
         if type_param:
+            supers = Super.objects.all()
             supers = supers.filter(super_type__type=type_param)
-        serializer = SuperSerializer(supers, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            serializer = SuperSerializer(supers, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            super_types = SuperType.objects.all()
+            custom_response_dictionary = {}
+            for super_type in super_types:
+                supers = Super.objects.filter(super_type_id=super_type.id)
+                super_serializer = SuperSerializer(supers, many=True)
+                custom_response_dictionary[super_type.type] = {
+                    "Supers": super_serializer.data,
+                }
+            return Response(custom_response_dictionary)
+
+
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)

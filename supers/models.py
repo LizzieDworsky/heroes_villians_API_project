2712c6f7b1796ca46aac1f03a239a11fa8309636
LizzieDworsky <amydworsky15@gmail.com>
2022-05-27 14:22:24 -0600

    edited the Super model to create a many to many relationship with Power model

--- supers/models.py
+++ supers/models.py
@@ -3,14 +3,20 @@ from super_types.models import SuperType
 
 # Create your models here.
 
+
+class Power(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Super(models.Model):
     name = models.CharField(max_length=255)
     alter_ego = models.CharField(max_length=255)
-    primary_ability = models.CharField(max_length=255)
-    secondary_ability = models.CharField(max_length=255)
+    powers = models.ManyToManyField(Power)
     catchphrase = models.CharField(max_length=255)
     super_type = models.ForeignKey(SuperType, default=1, on_delete=models.PROTECT)
 
 
-class Power(models.Model):
-    name = models.CharField(max_length=255)
+## Bonus 1:
+
+#- Alter the Super model to replace primary & secondary ability with a 'powers' ManyToManyField.
+#- Create a PATCH endpoint for the supers app that allows you to add a new Power to a Super by submitting the PK of the hero and the new power as path variables.
